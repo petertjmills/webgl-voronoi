c94be5fa84e8eda1c16bae06e339c1c0823f9e42
petertjmills <56031935+petertjmills@users.noreply.github.com>
2023-05-15 23:13:35 +0100

    Need to rethink how to store seed distance.

--- src/Voronoi.jsx
+++ src/Voronoi.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useState, useCallback } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import useMeasure from "react-use-measure";
+import { Perf } from "r3f-perf";
 
 import * as THREE from "three";
 
@@ -14,6 +15,7 @@ const shader = new THREE.ShaderMaterial({
 		},
 		u_mouse: { value: new THREE.Vector2(0.0, 0.0) },
 		u_seedPixels: { value: new THREE.DataTexture() },
+		u_stepSize: { value: 0.0 },
 	},
 	vertexShader: /*glsl*/ `        
         void main() {
@@ -25,38 +27,52 @@ const shader = new THREE.ShaderMaterial({
         uniform vec2 u_mouse;
         uniform float u_time;
         uniform sampler2D u_seedPixels;
-
+        uniform float u_stepSize;
+        
         void main() {
-            vec2 st = gl_FragCoord.xy/u_resolution.xy;
-            vec4 seed = texture2D(u_seedPixels, st);
-
-            // jump flood algorithm
-            u
+            vec2 p = gl_FragCoord.xy/u_resolution.xy;
+            vec4 pColour = texture2D(u_seedPixels, p);
+            // gl_FragColor = vec4(p, 0.0, 1.0);
+            //for each neighbor q at (x+i, y+j) where i,j = -stepSize, 0, stepSize
+            for (int x=-1; x<=1; x++) {
+                for (int y=-1; y<=1; y++) {
+                    vec2 q = vec2(p.x + float(x)*u_stepSize, p.y + float(y)*u_stepSize);
+                    vec4 qColour = texture2D(u_seedPixels, q);
+                    //gl_FragColor = qColour;
+                    if (pColour == vec4(0.0, 0.0, 0.0, 0.0)) {
+                        if (qColour != vec4(0.0, 0.0, 0.0, 0.0)) {
+                            float distance = distance(p, q);
+                            pColour = vec4(qColour.rgb, distance);
+                        }
+                    }
+                    if (pColour != vec4(0.0, 0.0, 0.0, 0.0)) {
+                        if (qColour != vec4(0.0, 0.0, 0.0, 0.0)) {
+                            if (pColour.a < qColour.a) {
+                                float distance = distance(p, q);
+                                pColour = vec4(qColour.rgb, distance);
+                            }
+                        }
+                    }
+                }
+            }
+            gl_FragColor = pColour;
 
         }
     `,
 });
 
-const Voronoi = ({ width, height }) => {
-	useFrame((state, delta, xrFrame) => {
-		shader.uniforms.u_time.value += delta;
-	});
-	//array 4xwidthxheight
+const createTexture = (width, height, seedPixels) => {
 	const data = new Uint8Array(width * height * 4);
 
-	//set 2 random pixels to red
-    for (let i = 0; i < 100; i++) {
-        const x = Math.floor(Math.random() * width);
-        const y = Math.floor(Math.random() * height);
-
-        const index = (x + y * width) * 4;
-        data[index] = Math.random() * 255;
-        data[index + 1] = Math.random() * 255;
-        data[index + 2] = 0;
-        data[index + 3] = Math.random() * 255;
-    }
-    console.log(data);
-
+	for (let i = 0; i < seedPixels.length; i++) {
+		const x = Math.floor(seedPixels[i].x * width);
+		const y = Math.floor(seedPixels[i].y * height);
+		const index = (x + y * width) * 4;
+		data[index] = Math.random() * 255;
+		data[index + 1] = Math.random() * 255;
+		data[index + 2] = 0;
+		data[index + 3] = 255;
+	}
 	const texture = new THREE.DataTexture(
 		data,
 		width,
@@ -65,16 +81,79 @@ const Voronoi = ({ width, height }) => {
 	);
 	texture.needsUpdate = true;
 
-	shader.uniforms.u_seedPixels.value = texture;
+	return texture;
+};
+
+const Voronoi = ({ width, height }) => {
+	const renderTargetA = new THREE.WebGLRenderTarget(width, height, {
+		format: THREE.RGBAFormat,
+	});
+	const renderTargetB = new THREE.WebGLRenderTarget(width, height, {
+		format: THREE.RGBAFormat,
+	});
+
+	const seedPixels = [
+		new THREE.Vector2(0.5, 0.5),
+		new THREE.Vector2(0.1, 0.1),
+	];
+	const seedPixelsTexture = createTexture(width, height, seedPixels);
+	
+
+	useFrame((state, delta, xrFrame) => {
+		shader.uniforms.u_time.value += delta;
+		const { gl, scene, camera } = state;
+
+        // shader.uniforms.u_seedPixels.value = seedPixelsTexture;
+		// shader.uniforms.u_stepSize.value = 1/1;
+        
+		// gl.setRenderTarget(renderTargetA);
+		// gl.render(scene, camera);
+		// gl.setRenderTarget(null);
+
+		// shader.uniforms.u_seedPixels.value = renderTargetA.texture;
+		// shader.uniforms.u_stepSize.value = 1/2;
+
+		// gl.setRenderTarget(renderTargetB);
+		// gl.render(scene, camera);
+		// gl.setRenderTarget(null);
+
+        // shader.uniforms.u_seedPixels.value = renderTargetB.texture;
+		// shader.uniforms.u_stepSize.value = 1/4;
+
+		let stepSize = 1.0;
+		shader.uniforms.u_stepSize.value = stepSize;
+		shader.uniforms.u_seedPixels.value = seedPixelsTexture;
+
+		let current = renderTargetA;
+		let next = renderTargetB;
+
+		gl.setRenderTarget(current);
+		gl.render(scene, camera);
+		gl.setRenderTarget(null);
+
+		while (stepSize > 1/4) {
+            stepSize /= 2.0;
+
+		    shader.uniforms.u_stepSize.value = stepSize;
+		    shader.uniforms.u_seedPixels.value = current.texture;
+
+		    gl.setRenderTarget(next);
+		    gl.render(scene, camera);
+		    gl.setRenderTarget(null);
+
+		    const temp = current;
+		    current = next;
+		    next = temp;
+		}
+
+	});
 
 	shader.uniforms.u_resolution.value = new THREE.Vector2(width, height);
-
 	return <mesh geometry={geometry} material={shader} />;
 };
 
 const App = (props) => {
 	const [backgroundColor, setBackgroundColor] = useState("#000000");
-
 	const [ref, bounds] = useMeasure();
 
 	const getURLparams = () => {
@@ -107,7 +186,11 @@ const App = (props) => {
 							premultipliedAlpha: false,
 						}}
 					>
-						<Voronoi height={bounds.height} width={bounds.width} />
+						<Perf />
+						<Voronoi
+							height={bounds.height * window.devicePixelRatio}
+							width={bounds.width * window.devicePixelRatio}
+						/>
 					</Canvas>
 				</div>
 			</div>
